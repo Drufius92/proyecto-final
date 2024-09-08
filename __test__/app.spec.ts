3db802f9f62509ea71fb52891aa20d54a5b2408e
Claudio Mora <claudiomoramendez@gmail.com>
2024-09-08 02:12:25 -0300

    incorporación de pruebas, cobertura del 100%

--- __test__/app.spec.ts
+++ __test__/app.spec.ts
@@ -5,29 +5,120 @@ import { configuration } from "../src/config.js";
 
 describe("Test Suite App", () => {
 
-    test("endpoint /", () => {
-        expect(1 + 1).toBe(2);
+    describe("Pruebas del endpoint raíz /", () => {
+
+        test("test de endpoint /", async () => {
+            return await request(app)
+                .get("/")
+                .expect("Content-Type", /text/)
+                .expect(200)
+                .then((response) => {
+                    expect(response.text).toBe(`Hola, esta api fue configurada por el usuario ${configuration.username}`);
+                });
+        });
     });
 
-    test("endpoint key", () => {
-        expect(1 + 1).toBe(2);
+    describe("Pruebas del endpoint /key", () => {
+        test("test de endpoint /key ", async () => {
+            return await request(app)
+                .get("/key")
+                .expect("Content-Type", /text/)
+                .expect(200)
+                .then((response) => {
+                    expect(response.text).toBe(`Hola, esta api contiene la siguiente api-key: ${configuration.apiKey}`);
+                });
+        });
     });
 
-    test("endpoint /palindromo", () => {
-        expect(1 + 1).toBe(2);
+    describe("Pruebas del endpoint /palindromo", () => {
+
+        test("debería volver es palindromo /palindromo/abccba", async () => {
+            let a = "abccba";
+            return await request(app)
+                .get("/palindromo/" + a)
+                .expect("Content-Type", /text/)
+                .expect(200)
+                .then((response) => {
+                    expect(response.text).toBe(`Hola, La frase ingresada es palindromo`);
+                });
+        });
+
+
+        test("debería volver es palindromo /palindromo/abc cba", async () => {
+            let a = "abc cba";
+            return await request(app)
+                .get("/palindromo/" + a)
+                .expect("Content-Type", /text/)
+                .expect(200)
+                .then((response) => {
+                    expect(response.text).toBe(`Hola, La frase ingresada es palindromo`);
+                });
+        });
+
+        test("debería volver no es palindromo /palindromo/abcabc", async () => {
+            let a = "abcabc";
+            return await request(app)
+                .get("/palindromo/" + a)
+                .expect("Content-Type", /text/)
+                .expect(200)
+                .then((response) => {
+                    expect(response.text).toBe(`Hola, La frase ingresada no es palindromo`);
+                });
+        });
+
     });
 
-    test("endpoint /primo", () => {
-        expect(1 + 1).toBe(2);
-    });
+    describe("Pruebas del endpoint /primo", () => {
+        test("debería devolver no primo para /primo/-1", async () => {
+            let a = -1;
+            return await request(app)
+                .get("/primo/" + a)
+                .expect("Content-Type", /text/)
+                .expect(200)
+                .then((response) => {
+                    expect(response.text).toBe(`Hola, el numero ingresado no es un numero primo`);
+                });
+        });
 
-    test("test de endpoint /", async () => {
-        return await request(app)
-            .get("/")
-            .expect("Content-Type", /text/)
-            .expect(200)
-            .then((response) => {
-                expect(response.text).toBe(`Hola, esta api fue configurada por el usuario ${configuration.username}`);
-            })
+        test("debería devolver no primo para /primo/1", async () => {
+            let a = 1;
+            return await request(app)
+                .get("/primo/" + a)
+                .expect("Content-Type", /text/)
+                .expect(200)
+                .then((response) => {
+                    expect(response.text).toBe(`Hola, el numero ingresado no es un numero primo`);
+                });
+        });
+
+        test("debería devolver primo para /primo/2", async () => {
+            let a = 2;
+            return await request(app)
+                .get("/primo/" + a)
+                .expect("Content-Type", /text/)
+                .expect(200)
+                .then((response) => {
+                    expect(response.text).toBe(`Hola, el numero ingresado es un numero primo`);
+                });
+        });
+
+        test("debería devolver no primo para /primo/4", async () => {
+            let a = 4;
+            return await request(app)
+                .get("/primo/" + a)
+                .expect("Content-Type", /text/)
+                .expect(200)
+                .then((response) => {
+                    expect(response.text).toBe(`Hola, el numero ingresado no es un numero primo`);
+                });
+        });
+        
+        
+
+        test("debería devolver 404 para /primo sin parámetro", async () => {
+            return await request(app)
+                .get("/primo")
+                .expect(404);
+        });
     });
 });
